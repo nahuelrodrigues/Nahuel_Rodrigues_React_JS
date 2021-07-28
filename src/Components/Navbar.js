8bef8c7e163e007abadaa4c712315ac670f01749
Nahuel <rodriguesnahuel@gmail.com>
2021-07-27 21:31:24 -0300

    menu funcionando

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+//AFTERRR !
+
+// import { Navbar } from "react-bootstrap";
+// import React from "react";
+
+// export const Navbar = () => {
+//   return (
+//     <div>
+//       <h1>hola</h1>
+//     </div>
+//   );
+// };
+
+// export default Navbar;

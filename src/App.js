87ae9ed4a1cbdeae57386158dbff8a601a5685a4
Nahuel <rodriguesnahuel@gmail.com>
2021-07-31 17:25:44 -0300

    reorganización de los componentes y app.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "./Components/Navbar";
+
+function App() {
+  return <NavBar></NavBar>;
+}
+export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "../App.css";
 import logo from "../img/logo.png";
 
-function App() {
+function NavBar() {
   return (
     <div className="App">
       <Navbar
@@ -41,31 +41,7 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div className="content">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
-          minus molestiae vel beatae natus eveniet ratione temporibus aperiam
-          harum alias officiis assumenda officia quibusdam deleniti eos
-          cupiditate dolore doloribus!
-        </p>
-        <p>
-          Ad dolore dignissimos asperiores dicta facere optio quod commodi nam
-          tempore recusandae. Rerum sed nulla eum vero expedita ex delectus
-          voluptates rem at neque quos facere sequi unde optio aliquam!
-        </p>
-        <p>
-          Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur
-          porro quaerat autem ipsam odit quam beatae tempora quibusdam illum!
-          Modi velit odio nam nulla unde amet odit pariatur at!
-        </p>
-        <p>
-          Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto
-          nihil explicabo perferendis quos provident delectus ducimus
-          necessitatibus reiciendis optio tempora unde earum doloremque commodi
-          laudantium ad nulla vel odio?
-        </p>
-      </div>
     </div>
   );
 }
-export default App;
+export default NavBar;
